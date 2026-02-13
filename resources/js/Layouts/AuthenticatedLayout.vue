7bd1e6f11974c66c919af5eeb3f1e5ea4818f731
Sam Soliman <msthea2@gmail.com>
2026-02-13 16:36:06 +0800

    02-13a

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -85,6 +85,7 @@ const toggleDarkMode = () => {
                                 <template #content>
                                     <DropdownLink
                                         :href="route('profile.edit')"
+                                        class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-700"
                                     >
                                         Profile
                                     </DropdownLink>
@@ -92,6 +93,7 @@ const toggleDarkMode = () => {
                                         :href="route('logout')"
                                         method="post"
                                         as="button"
+                                        class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-700"
                                     >
                                         Log Out
                                     </DropdownLink>
